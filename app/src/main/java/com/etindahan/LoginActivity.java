fed35f5815e21e7929bd42d3e5c4d3391ea1ebf9
Justin Ruzol <justindaleruzol@gmail.com>
2019-05-31 23:26:03 +0800

    Added Login Function (Incomplete registration)

--- app/src/main/java/com/etindahan/LoginActivity.java
+++ app/src/main/java/com/etindahan/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.etindahan;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -73,6 +74,15 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reg = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(reg);
+            }
+        });
+
 
     }
 
