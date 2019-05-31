93a2e86c65d25aa6dcb001a7d258f186c9ffe82d
Justin Ruzol <justindaleruzol@gmail.com>
2019-05-31 23:05:14 +0800

    Added Login Function (No registration yet)

--- app/src/main/java/com/etindahan/LoginActivity.java
+++ app/src/main/java/com/etindahan/LoginActivity.java
@@ -1,25 +1,101 @@
 package com.etindahan;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-
+    private Button Login;
+    private EditText Email, Password;
+    private TextView Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Login = (Button) findViewById(R.id.SigninButton);
+        Email = (EditText) findViewById(R.id.EmailField);
+        Password = (EditText) findViewById(R.id.PasswordField);
+        Register = (TextView) findViewById(R.id.RegisterText);
+
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
-        //Hong was here :D Ced too
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                String email = Email.getText().toString();
+                String password = Password.getText().toString();
+
+                if(email == null && password == null){
+                    Toast.makeText(LoginActivity.this, "Please complete the field!",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else if (email.length() <= 6 && password.length() <= 6){
+                    Toast.makeText(LoginActivity.this, "Please complete the field!",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (!task.isSuccessful()) {
+                                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        checkIfEmailVerified();
+                                    }
+
+                                    // ...
+                                }
+                            });
+                    }
+
+
+
+            }
+        });
 
     }
+
+    //Henllo! Di pa tapos tong Login :D
+
+    private void checkIfEmailVerified() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user.isEmailVerified())
+        {
+            // user is verified, so you can finish this activity or send user to activity which you want.
+            finish();
+            Toast.makeText(LoginActivity.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            // email is not verified, so just prompt the message to the user and restart this activity.
+            // NOTE: don't forget to log out the user.
+            FirebaseAuth.getInstance().signOut();
+            //restart this activity
+
+        }
+    }
+
+
 }
