3aad155b9dabc7962ca35654498ef358245e1b91
Justin Ruzol <justindaleruzol@gmail.com>
2019-06-07 02:07:19 +0800

    Enabled SplashScreen (Asks for location permission. Will not run unless granted)

--- app/src/main/java/com/etindahan/SplashScreen.java
+++ app/src/main/java/com/etindahan/SplashScreen.java
@@ -1,9 +1,21 @@
 package com.etindahan;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 1300;
+    private final int REQUEST_ACCESS_FINE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +23,47 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
 
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermission();
+        }
+        else if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Granted();
+        }
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == REQUEST_ACCESS_FINE){
+            if(grantResults.length> 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Granted();
+            }
+            else {
+                Denied();
+            }
+        }
+
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION},REQUEST_ACCESS_FINE);
+    }
+
+    public void Granted(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    public void Denied(){
+        Toast.makeText(SplashScreen.this, "Location Access is required!", Toast.LENGTH_LONG).show();
+        recreate();
+    }
+
 }
