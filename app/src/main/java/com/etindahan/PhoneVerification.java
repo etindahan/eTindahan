694baec65aedf67b541d971c4e5b9fde30c71b28
Snapalapagus <thyname23>
2019-06-29 17:48:38 +0800

    SMS Verification

---
+++ app/src/main/java/com/etindahan/PhoneVerification.java
@@ -0,0 +1,110 @@
+package com.etindahan;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class PhoneVerification extends AppCompatActivity {
+
+    private String verificationid;
+    private FirebaseAuth mAuth;
+    private EditText verifytext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_verification);
+
+        mAuth = FirebaseAuth.getInstance();
+        verifytext = findViewById(R.id.verifycode);
+        String phoneNummber = getIntent().getStringExtra("phonenumber");
+
+     sendVerificationCode(phoneNummber);
+
+     findViewById(R.id.VerifyButton).setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             String code = verifytext.getText().toString().trim();
+             if(code.isEmpty() || code.length() <6 ){
+                 verifytext.setError("Enter correct code");
+                 verifytext.requestFocus();
+                 return;
+             }
+         }
+     });
+    }
+    private void VerifyCode(String code){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationid, code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(PhoneVerification.this, ProfileFragment.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+
+                        }else{
+                            Toast.makeText(PhoneVerification.this, task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationCode(String number) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                number,
+                60,
+                TimeUnit.SECONDS,
+                TaskExecutors.MAIN_THREAD,
+                mCallBack
+        );
+
+
+    }
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationid = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if(code != null)
+            {
+                signInWithCredential(phoneAuthCredential);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(PhoneVerification.this, e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+    };
+}
+
+
+
+
+
