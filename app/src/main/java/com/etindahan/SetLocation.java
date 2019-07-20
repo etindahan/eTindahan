4cc2f8bf362f19b91a486ec17f911640906711b8
Justin Ruzol <justindaleruzol@gmail.com>
2019-07-20 22:40:36 +0800

    Fixed Login menu fields. Added PIN location on SetLocation Activity.

--- app/src/main/java/com/etindahan/SetLocation.java
+++ app/src/main/java/com/etindahan/SetLocation.java
@@ -2,11 +2,14 @@ package com.etindahan;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -14,23 +17,28 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 public class SetLocation extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Button setLocationButton;
+    String Location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_location);
+
+        setLocationButton = findViewById(R.id.set_locationButton);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
     }
 
     @Override
@@ -38,13 +46,6 @@ public class SetLocation extends FragmentActivity implements OnMapReadyCallback 
         mMap = googleMap;
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
 
@@ -67,27 +68,47 @@ public class SetLocation extends FragmentActivity implements OnMapReadyCallback 
             }
         });
 
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+    //    mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+    //        @Override
+    //        public void onMapClick(LatLng latLng) {
+    //          // Creating a marker
+    //          MarkerOptions markerOptions = new MarkerOptions();
+    //
+    //          // Setting the position for the marker
+    //          markerOptions.position(latLng);
+    //
+    //          // Setting the title for the marker.
+    //          // This will be displayed on taping the marker
+    //          markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+    //
+    //          // Clears the previously touched position
+    //          mMap.clear();
+    //
+    //          // Animating to the touched position
+    //          mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+    //
+    //            // Placing a marker on the touched position
+    //            mMap.addMarker(markerOptions);
+    //        }
+    //    });
+
+        mMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
             @Override
-            public void onMapClick(LatLng latLng) {
-                // Creating a marker
-                MarkerOptions markerOptions = new MarkerOptions();
+            public void onCameraIdle() {
 
-                // Setting the position for the marker
-                markerOptions.position(latLng);
-
-                // Setting the title for the marker.
-                // This will be displayed on taping the marker
-                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
-
-                // Clears the previously touched position
                 mMap.clear();
 
-                // Animating to the touched position
-                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                Log.i("CenterLat", String.valueOf(mMap.getCameraPosition().target.latitude));
+                Log.i("CenterLong",String.valueOf(mMap.getCameraPosition().target.longitude));
 
-                // Placing a marker on the touched position
-                mMap.addMarker(markerOptions);
+                Location = mMap.getCameraPosition().target.latitude + "" + mMap.getCameraPosition().target.longitude;
+
+                setLocationButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(SetLocation.this, Location, Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
     }
